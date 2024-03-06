d4a3c77ba9e0a205a15c8430539f01796ddc0273
Alastair Fraser <alastairfraser.work@gmail.com>
2024-03-06 23:10:44 +0100

    Feat: 23:10

--- client/src/components/Loading/Loading.jsx
+++ client/src/components/Loading/Loading.jsx
@@ -1,9 +1,21 @@
 import "./Loading.css";
-
 import React from "react";
+import Lottie from "react-lottie";
+import animationData from "./logo-animation.json";
+import animationTwoData from "./logo-animation 2.json";
 import { motion, AnimatePresence } from "framer-motion";
 
 export function Loading() {
+
+const defaultOptions = {
+  loop: true,
+  autoplay: true,
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: "xMidYMid slice",
+  },
+};
+
   return (
     <AnimatePresence>
       <motion.div
@@ -12,9 +24,8 @@ export function Loading() {
         exit={{ opacity: 0 }}
       >
         <div className="load-wrap">
-      
+          <Lottie options={defaultOptions} height={600} width={600} />
         </div>
-
       </motion.div>
     </AnimatePresence>
   );
